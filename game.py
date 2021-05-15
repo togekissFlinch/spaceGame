6b8b499c1bb24c5ca111ac9492dc1ad58278541e
RajPorus19 <raj.porus.hiruthayaraj@gmail.com>
2021-05-15 21:53:00 +0200

    régler le problème de la flamme

--- game.py
+++ game.py
@@ -3,7 +3,6 @@ import time
 #fenêtre
 window = t.Screen()
 window.bgpic("background.gif")
-enemy_is = "NOT KILLED"
 #importer les sprites
 t.register_shape("player.gif")
 t.register_shape("player2.gif")
@@ -13,6 +12,9 @@ t.register_shape("fire3.gif")
 t.register_shape("fire4.gif")
 t.register_shape("alien.gif")
 t.register_shape("alien2.gif")
+t.register_shape("alien3.gif")
+t.register_shape("alien4.gif")
+t.register_shape("alien5.gif")
 #joueur
 player = t.Turtle()
 player.shape("player.gif")
@@ -24,11 +26,12 @@ fire.shape("fire1.gif")
 fire.penup()
 #enemy
 enemy = t.Turtle()
-enemy.shape("alien2.gif")
+enemy.shape("alien.gif")
 enemy.hideturtle()
 enemy.penup()
 enemy.setposition(-400,-40)
 enemy.showturtle()
+enemy_is ="NOT KILLED"
 #mouvements
 def right():
     x = player.xcor() 
@@ -36,8 +39,6 @@ def right():
     player.shape("player.gif")
     if x > 400:
         x = 400
-    elif x == ex:
-        enemy.hideturtle()
     x += 10
     player.setx(x)
     fire.setx(x)
@@ -47,16 +48,27 @@ def left():
     player.shape("player2.gif")
     if x < -400:
         x = -400
-    elif x == ex:
-        enemy.hideturtle()
     x -= 10
     player.setx(x)
     fire.setx(x)
 t.listen()
 t.onkey(right, "Right")
 t.onkey(left, "Left")
-#boucle
+#boucle & animation
 while True:
+    x = player.xcor()
+    ex = enemy.xcor()
+    if x == ex + 40:
+        if enemy_is == "NOT KILLED":
+            enemy_is = "KILLED"
+            enemy.shape("alien2.gif")
+            time.sleep(0.1)
+            enemy.shape("alien3.gif")
+            time.sleep(0.1)
+            enemy.shape("alien4.gif")
+            time.sleep(0.1)
+            enemy.shape("alien5.gif")
+            time.sleep(0.1)
     fire.shape("fire1.gif")
     time.sleep(0.1)
     fire.shape("fire2.gif")
