91360ecfe2fcefb811e267f6850fd9320e5be773
RajPorus19 <raj.porus.hiruthayaraj@gmail.com>
2021-05-15 14:44:56 +0200

    feu

--- game.py
+++ game.py
@@ -3,11 +3,16 @@ import time
 #fenêtre
 window = t.Screen()
 window.bgpic("background.gif")
+enemy_is = "NOT KILLED"
 #importer les sprites
 t.register_shape("player.gif")
 t.register_shape("player2.gif")
-t.register_shape("fire.gif")
+t.register_shape("fire1.gif")
+t.register_shape("fire2.gif")
+t.register_shape("fire3.gif")
+t.register_shape("fire4.gif")
 t.register_shape("alien.gif")
+t.register_shape("alien2.gif")
 #joueur
 player = t.Turtle()
 player.shape("player.gif")
@@ -15,11 +20,11 @@ player.penup()
 player.setposition(0,90)
 #fire
 fire = t.Turtle()
-fire.shape("fire.gif")
+fire.shape("fire1.gif")
 fire.penup()
 #enemy
 enemy = t.Turtle()
-enemy.shape("alien.gif")
+enemy.shape("alien2.gif")
 enemy.hideturtle()
 enemy.penup()
 enemy.setposition(-400,-40)
@@ -31,8 +36,8 @@ def right():
     player.shape("player.gif")
     if x > 400:
         x = 400
-    if x == ex:
-        enemy.shape("fire.gif")
+    elif x == ex:
+        enemy.hideturtle()
     x += 10
     player.setx(x)
     fire.setx(x)
@@ -42,8 +47,8 @@ def left():
     player.shape("player2.gif")
     if x < -400:
         x = -400
-    if x == ex:
-        enemy.shape("fire.gif")
+    elif x == ex:
+        enemy.hideturtle()
     x -= 10
     player.setx(x)
     fire.setx(x)
@@ -51,5 +56,12 @@ t.listen()
 t.onkey(right, "Right")
 t.onkey(left, "Left")
 #boucle
-t.mainloop()
-
+while True:
+    fire.shape("fire1.gif")
+    time.sleep(0.1)
+    fire.shape("fire2.gif")
+    time.sleep(0.1)
+    fire.shape("fire3.gif")
+    time.sleep(0.1)
+    fire.shape("fire4.gif")
+    time.sleep(0.1)
